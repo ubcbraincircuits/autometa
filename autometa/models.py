6527f8de1f0113c0913b0e439e4cb14d6bb9d834
Ashutosh Bhudia <ashu.bhudia@gmail.com>
2019-07-30 10:20:27 -0700

    Add Dropdown class

--- autometa/models.py
+++ autometa/models.py
@@ -58,3 +58,57 @@ class Field(ABC):
     @abstractmethod
     def from_json(self, json_dump):
         pass
+
+
+class Dropdown(Field):
+    def init(self):
+        self._choices = set()
+        self._current_selection = None
+        return self
+
+    @property
+    def choices(self):
+        return self._choices
+
+    def add_choice(self, choice):
+        self._choices.add(choice)
+
+    def remove_choice(self, choice):
+        self._choices.remove(choice)
+
+    def clear_all_choices(self):
+        self._choices.clear()
+
+    @property
+    def current_selection(self):
+        return self._current_selection
+
+    @current_selection.setter
+    def current_selection(self, selection):
+        self._current_selection = selection
+
+    def to_json(self):
+        name = self._name
+        if (name == None) or (name == ""):
+            raise ValueError("Attribute ``name`` not specified")
+        description = self._description
+        if (description == None) or (description == ""):
+            raise ValueError("No description given")
+        choices = list(self.choices)
+        if len(choices) == 0:
+            raise ValueError("There are no dropdown options for ``{}``".format(name))
+        attributes = {
+            "type": "Dropdown",
+            "name": name,
+            "description": description,
+            "choices": choices,
+            "required": self.required,
+        }
+        return json.dumps(attributes)
+
+    def from_json(self, json_dump):
+        loaded_dump = json.loads(json_dump)
+        self._name = loaded_dump["name"]
+        self._descripton = loaded_dump["description"]
+        self._choices = set(loaded_dump["choices"])
+        self._required = loaded_dump["required"]
